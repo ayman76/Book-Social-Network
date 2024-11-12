7d0e109b9095b3d8af3028b0398d7eef2f4c1c8c
ayman <a.m_ayman9@outlook.com>
2024-11-12 07:06:26 +0200

    Adding Book and Feedback Entities

---
+++ src/main/java/com/example/book_social_netwok/book/Book.java
@@ -0,0 +1,34 @@
+package com.example.book_social_netwok.book;
+
+import com.example.book_social_netwok.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Book  extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+}
---
+++ src/main/java/com/example/book_social_netwok/feedback/Feedback.java
@@ -0,0 +1,29 @@
+package com.example.book_social_netwok.feedback;
+
+import com.example.book_social_netwok.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note; // 1-5 stars
+    private String comment;
+
+}
